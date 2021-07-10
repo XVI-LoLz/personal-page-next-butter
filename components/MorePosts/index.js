c3bcd2c9a99f79f1266d2a5b0b71fa01d85e85dd
Elihu Munguía <47308963+XVI-LoLz@users.noreply.github.com>
2021-07-10 02:30:07 -0500

    create: New components and style components

---
+++ components/MorePosts/index.js
@@ -0,0 +1,10 @@
+import PostCard from "components/PostCard";
+
+export default function MorePosts({ posts }) {
+  return (
+    <div className="posts-container">
+      {posts.length > 0 &&
+        posts.map((post) => <PostCard key={post.slug} {...post} />)}
+    </div>
+  );
+}
---
+++ components/PostTitle/index.js
@@ -0,0 +1,5 @@
+import { Header } from "styled-components";
+
+export default function PostTitle({ children }) {
+  return <Header>{children}</Header>;
+}
