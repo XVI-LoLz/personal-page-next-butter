0579d69f867f29ce773cb6abda7f12c89380ad21
Elihu Munguía <47308963+XVI-LoLz@users.noreply.github.com>
2021-08-06 01:06:51 -0500

    refactor: MainNavbar to use new components

--- components/MainNavbar/index.js
+++ components/MainNavbar/index.js
@@ -1,60 +1,22 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-import cn from "classnames";
-import useTranslation from "next-translate/useTranslation";
-import { FaBlog, FaBookReader, FaHome } from "react-icons/fa";
+import Logo from "public/logo.svg";
 
 import Socials from "components/Socials";
+import Locales from "components/Locales";
+import Routes from "./Routes";
+import Hamburger from "./Hamburger";
 
-import Logo from "public/logo.svg";
 import style from "./style.module.scss";
 
 export default function MainNavbar() {
-  const { pathname } = useRouter();
-  const { t } = useTranslation("common");
-
-  const routes = [
-    {
-      route: "/",
-      label: t`navbar.home`,
-      icon: FaHome,
-    },
-    {
-      route: "/blog",
-      label: t`navbar.blog`,
-      icon: FaBlog,
-    },
-    {
-      route: "/resources",
-      label: t`navbar.resources`,
-      icon: FaBookReader,
-    },
-  ];
-
   return (
     <nav className={style.MainNavbar}>
       <Logo className={style.logo} />
-      <ul className={style.links}>
-        {routes?.map(({ route, label, icon: Icon }, i) => (
-          <li key={i}>
-            <Link href={route}>
-              <a
-                className={cn(style.link, {
-                  [style.active]:
-                    i === 0
-                      ? pathname.length === route.length
-                      : pathname.startsWith(route),
-                })}
-                title={label}
-              >
-                <span className={style.label}>{label}</span>
-                <Icon className={style.icon} />
-              </a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <Socials />
+      <Routes style={style} />
+      <div className={style.right}>
+        <Socials />
+        <Locales />
+        <Hamburger />
+      </div>
     </nav>
   );
 }
