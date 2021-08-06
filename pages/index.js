912237721bd3e700245405a81b7de31290f61f37
Elihu Munguía <47308963+XVI-LoLz@users.noreply.github.com>
2021-08-06 01:05:35 -0500

    remove: only locale content in home page

--- pages/index.js
+++ pages/index.js
@@ -44,9 +44,8 @@ const Sidebar = ({ projects = [] }) => {
   );
 };
 
-export async function getStaticProps({ preview = null, locale = "es" }) {
-  const slugLocale = locale.toLowerCase();
-  const allPosts = (await getAllPostsForHome(preview, slugLocale)) || [];
+export async function getStaticProps({ preview = null }) {
+  const allPosts = (await getAllPostsForHome(preview)) || [];
   const projects = (await getProjectsForHome()) || [];
   return {
     props: { allPosts, preview, projects },
